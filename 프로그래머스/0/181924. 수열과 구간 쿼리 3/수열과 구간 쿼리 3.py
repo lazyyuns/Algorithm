5438e8b2d57de21d9e905106cb4534377ea6aba7
lazyyuns <88392352+lazyyuns@users.noreply.github.com>
2024-07-25 19:09:43 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.17 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,5 +1,4 @@
 def solution(arr, queries):
-    for i in queries:
-        arr[i[0]], arr[i[1]] = arr[i[1]], arr[i[0]]
-    
+    for i, j in queries:
+        arr[i], arr[j] = arr[j], arr[i]
     return arr
