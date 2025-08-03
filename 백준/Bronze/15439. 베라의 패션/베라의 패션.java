edeff2a3bdc8861a0a6a02a03b928d41176bcce6
lazyyuns <88392352+lazyyuns@users.noreply.github.com>
2025-08-03 18:04:05 +0900

    [Bronze IV] Title: 베라의 패션, Time: 104 ms, Memory: 12888 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int answer = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                answer += 1;
+            }
+        }
+        System.out.println(answer-n);
+    }
+}
