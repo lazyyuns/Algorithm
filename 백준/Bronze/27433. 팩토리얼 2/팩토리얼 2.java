4f4b8e076b9e48707da3612048e16ad19cfcf3cd
lazyyuns <88392352+lazyyuns@users.noreply.github.com>
2025-08-03 15:23:54 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 92 ms, Memory: 12832 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        long result  = factorial(n);
+        System.out.println(result);
+        
+    }
+    
+    public static long factorial(int n){
+        if(n <= 1) return 1;
+        else{
+            return n * factorial(n-1);
+        }
+    }
+}
