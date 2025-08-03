3ee411fe9d45e852ab27445fde71005c894703da
lazyyuns <88392352+lazyyuns@users.noreply.github.com>
2025-08-03 19:42:05 +0900

    [Silver IV] Title: 제로, Time: 176 ms, Memory: 21308 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> st = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int i = 0 ; i < n; i++){
+            int k = Integer.parseInt(br.readLine());
+            if(k != 0) st.push(k);
+            else st.pop();
+        }
+        
+        int sum = 0;
+         while(!st.isEmpty()){
+            sum += st.pop();
+        }
+            
+        
+        System.out.println(sum);
+        
+    }
+}
