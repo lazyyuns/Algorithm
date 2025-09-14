35e32d25e9a2b0e40ee23ac8e4461e29d48acb8d
lazyyuns <88392352+lazyyuns@users.noreply.github.com>
2025-09-14 17:06:19 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 13196 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-public class Main{
+public class Main {
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
@@ -15,9 +15,9 @@ public class Main{
     if(n >= 2){
       dp[2] = dp[1] + stairs[2];
     }
-    
-    for(int i = 3; i <= n; i++){
-      dp[i] = Math.max(dp[i-2] + stairs[i], dp[i-3]+ stairs[i-1] + stairs[i]);
+
+    for(int i = 3 ; i <= n; i++){
+      dp[i] = Math.max(dp[i-2] + stairs[i], dp[i-3]+stairs[i-1]+stairs[i]);
     }
 
     System.out.println(dp[n]);
